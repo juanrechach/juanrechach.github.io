ac2602c659486d31a20849baabdba05c129c38cc
Juan Rechach <juanrechach@gmail.com>
2024-12-19 17:05:32 +0100

    Add new screens

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "801515944"
+    serviceWorkerVersion: "4176145333"
   }
 });
